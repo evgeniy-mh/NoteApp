e7c99c328a1340913e8d968df26460b1cd5f0d50
evgeniy-mh <djpon94@gmail.com>
2018-12-20 19:14:02 +0300

    added api folder for remote server

---
+++ api/get_notes.php
@@ -0,0 +1,22 @@
+<?php
+require 'server_params.php';
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$notesDbName", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+$stmt = $conn->prepare("SELECT * FROM Notes");
+    $stmt->execute();
+    
+    $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    $json=json_encode($result);
+    echo($json);
+
+    }
+catch(PDOException $e)
+    {
+    http_response_code(500);
+    echo "Error: " . $e->getMessage();
+    }
+    $conn = null;
+?> 
---
+++ api/server_params.php
@@ -0,0 +1,7 @@
+<?php
+    $servername = "sql300.epizy.com";
+    $username = "epiz_23166104";
+    $password = "7DO27Z3bDwa"; 
+    
+    $notesDbName="epiz_23166104_notesDB";
+?>
