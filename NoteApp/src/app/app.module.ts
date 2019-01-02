19c3fd8aa2857d067192e335f52b9d70aea85dfb
evgeniy-mh <djpon94@gmail.com>
2019-01-02 20:18:59 +0300

    added routing added sign in component

--- NoteApp/src/app/app.module.ts
+++ NoteApp/src/app/app.module.ts
@@ -1,26 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
 import { NoteComponent } from './note/note.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+const appRoutes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'all-notes', component: NotesComponent },
+  { path: '',
+    redirectTo: '/all-notes',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesComponent,
-    NoteComponent
+    NoteComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
